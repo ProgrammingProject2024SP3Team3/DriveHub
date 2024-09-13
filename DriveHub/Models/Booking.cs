0b6d1bd303fb54f17e62c2983042515616a7cce7
Sean Atherton <sean@atho.com.au>
2024-09-14 09:17:44 +1000

    Introduce Booking model to manage car reservation details, linking users, vehicles, and locations.

---
+++ DriveHub/Models/Booking.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Booking
+{
+    [Key]
+    public Guid BookingID { get; set; }
+
+    [ForeignKey("AspNetUser")]
+    public string UserID { get; set; }
+
+    [ForeignKey("Vehicle")]
+    public Guid VehicleID { get; set; }
+
+    [ForeignKey("Location")]
+    public Guid StartLocationID { get; set; }
+
+    [ForeignKey("Location")]
+    public Guid EndLocationID { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    // Navigation properties
+    public AspNetUser User { get; set; }
+    public Vehicle Vehicle { get; set; }
+    public Location StartLocation { get; set; }
+    public Location EndLocation { get; set; }
+
+    // One-to-One relationship: A Booking generates one Receipt
+    public Receipt Receipt { get; set; }
+}
