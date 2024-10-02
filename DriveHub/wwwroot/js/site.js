b4094fc21ea5f188e6a2526dfebb61ac554a4323
Jack Strain <jack.strain@gmail.com>
2024-10-02 11:51:55 +0800

    Landing page: slight javascript fix so as not to error on other pages

--- DriveHub/wwwroot/js/site.js
+++ DriveHub/wwwroot/js/site.js
@@ -3,23 +3,26 @@
 
 // Write your JavaScript code.
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     const heroHeading = document.querySelector('.hero-heading');
-   
-    setTimeout(() => {
-        heroHeading.classList.add('scrolled');
-      },500);
 
-      const fadeInOnScroll = () => {
+    // A check if heroHeading exists so it doesn't error on other pages
+    if (heroHeading) {
+        setTimeout(() => {
+            heroHeading.classList.add('scrolled');
+        }, 500);
+    }
+
+    const fadeInOnScroll = () => {
         const scrollPosition = window.scrollY;
         const opacity = Math.min(1, scrollPosition / (windowHeight / 2));
-       
+
         heroHeading.style.opacity = opacity;
 
         if (scrollPosition <= windowHeight) {
             heroHeading.style.transition = 'opacity 0.5s ease-out';
         }
-      };
+    };
 
-window.addEventListener('scroll', fadeInOnScroll) 
+    window.addEventListener('scroll', fadeInOnScroll)
 });
