7e3d37606f697dc1c2607c27db70400afe16b1ff
Sean Atherton <sean@atho.com.au>
2024-10-05 15:51:44 +1000

    Set up xUnit test framework and project references

--- DriveHubTests/BookingTest.cs
+++ DriveHubTests/BookingTest.cs
@@ -1,3 +1,18 @@
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using DriveHub.Controllers;
+using DriveHubModel;
+using DriveHub.Data;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DriveHub.Models.ViewModels;
+using Moq;
+using Microsoft.Extensions.Options;
+
 namespace DriveHubTests
 {
     public class BookingTest
