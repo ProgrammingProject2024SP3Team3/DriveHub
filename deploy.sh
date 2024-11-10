5e8b93829d0d793910b500ab0ae55092f3469d3d
Jove2001 <s3863018@student.rmit.edu.au>
2024-11-10 18:15:26 +1100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/Jove2001/DriveHub.git
-cd DriveHub
 docker build -t drivehub-image -f DriveHubDocker .
 docker build -t admin-image -f AdminDocker .
 docker compose up -d
